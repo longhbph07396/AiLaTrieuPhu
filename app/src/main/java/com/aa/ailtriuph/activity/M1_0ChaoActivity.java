b248d4d327bb8cec96c9f456d7baa797952387a4
longhbph07396 <longhbph07396@fpt.edu.vn>
2020-02-16 04:57:13 +0700

    Code giao diện + animation đến m4

--- app/src/main/java/com/aa/ailtriuph/activity/M1_0ChaoActivity.java
+++ app/src/main/java/com/aa/ailtriuph/activity/M1_0ChaoActivity.java
@@ -3,28 +3,62 @@ package com.aa.ailtriuph.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.aa.ailtriuph.R;
+import com.aa.ailtriuph.base.BaseActivity;
 
-public class M1_0ChaoActivity extends AppCompatActivity implements View.OnClickListener {
+public class M1_0ChaoActivity extends BaseActivity implements View.OnClickListener {
+    private ImageView imgvIcon;
 
-    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        startAnimation();
+
     }
 
+    private void startAnimation() {
+        Animation animation = makeAnimation(R.anim.m1_0_ic_xoay_zoom, imgvIcon);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+               chuyenMHmakeTime(M2_0LoginActivity.class ,500);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+    }
+
+    //Ánh xạ các thành phần
     private void initView() {
-        button=findViewById(R.id.btnTesst);
-        button.setOnClickListener(this);
+        imgvIcon = (ImageView) findViewById(R.id.imgvIcon);
     }
 
     @Override
     public void onClick(View v) {
 
     }
+
+
 }
